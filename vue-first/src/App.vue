121e99e03009ec1d1364eb5e9bfb814744b511e3
freeefly <freefly3557@gmail.com>
2021-12-02 01:06:03 +0900

    feat: add provide and inject

--- vue-first/src/App.vue
+++ vue-first/src/App.vue
@@ -1,26 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Friend
+        :friend="friend"
+        :name="friend.name"
+        :phone-number="friend.phoneNumber"
+        email-address="freeefly1@host.com"
+        is-favorite="0"
+        @toggle-favorite="toggleFavoriteStatus"
+    />
+    <Friend name="freeefly2" phone-number="01213 12312" email-address="freeefly2@host.com"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Friend from "@/Friend";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  components: {Friend},
+  data() {
+    return {
+      friend: {
+        name: 'freeefly',
+        phoneNumber: '01012341234'
+      }
+    }
+  },
+  provide() {
+    return {
+      friend: this.friend
+    }
+  },
+
+  method: {
+    toggleFavoriteStatus(friendId) {
+      const foundFriend = this.friends.find(friend => friend.id === friendId);
+      foundFriend.isFavorite = !foundFriend.isFavorite;
+
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
+
 </style>
---
+++ vue-first/src/NewFriend.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "NewFriend"
+}
+</script>
+
+<style scoped>
+
+</style>
