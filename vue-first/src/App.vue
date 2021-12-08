3f28c8a39ab4aa4868bd035646e751767cf9248a
freeefly <freefly3557@gmail.com>
2021-12-09 00:53:51 +0900

    feat: test form examples

--- vue-first/src/App.vue
+++ vue-first/src/App.vue
@@ -1,57 +1,28 @@
 <template>
-  <the-header></the-header>
-  <badge-list></badge-list>
-  <div>
-    <h2>Manage Goals</h2>
-    <input type="text" v-model="goal">
-    <button @click="setGoal">Set Goal</button>
-    <teleport to="body">
-      <ErrorAlert v-if="inputIsInvalid">
-        <h2>Input is Invalid!</h2>
-        <p>Please enter at least a few characters...</p>
-      </ErrorAlert>
-    </teleport>
-  </div>
+  <the-form></the-form>
 </template>
 
 <script>
-
-import TheHeader from "@/components/TheHeader";
-import BadgeList from "@/components/BadgeList";
-import UserInfo from "@/components/UserInfo";
-import ErrorAlert from "@/components/ErrorAlert";
+import TheForm from './components/TheForm.vue';
 
 export default {
-  components: {ErrorAlert, TheHeader, BadgeList, UserInfo},
-  data() {
-    return {
-      goal: '',
-      inputIsInvalid: false,
-      selectedComponent: 'active-goals',
-      activeUser: {
-        name: 'Maximilian Schwarzmüller',
-        description: 'Site owner and admin',
-        role: 'admin',
-      },
-    };
-  },
-  method: {
-    setGoal() {
-      if (this.goal) {
-        this.inputIsInvalid = true;
-      }
-
-    }
+  components: {
+    TheForm
   }
-};
+}
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+
 html {
   font-family: sans-serif;
 }
 
 body {
   margin: 0;
+  background-color: #292929;
 }
 </style>
---
+++ vue-first/src/components/RatingControl.vue
@@ -0,0 +1,68 @@
+<template>
+  <ul>
+    <li>
+      <button type="button" :class="{active: modelValue === 'poor'}" @click="activate('poor')">Poor</button>
+    </li>
+    <li>
+      <button type="button" :class="{active: modelValue === 'average'}" @click="activate('average')">Average
+      </button>
+    </li>
+    <li>
+      <button type="button" :class="{active: modelValue === 'great'}" @click="activate('great')">Great</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "RatingControl",
+  props: ['modelValue'],
+  emits: ['update:modelValue'],
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    activate(option) {
+      this.$emit('update:modelValue', option);
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.active {
+  border-color: #a00078;
+}
+
+button.active  {
+  color: #a00078;
+}
+ul {
+  list-style: none;
+  margin: 0.5rem 0;
+  padding: 0;
+  display: flex;
+}
+
+li {
+  margin: 0 1rem;
+  border: 1px solid #ccc;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+button {
+  font: inherit;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+}
+
+
+</style>
